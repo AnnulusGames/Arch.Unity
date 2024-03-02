1928b33888ef2271290150f7b6418b5d62277877
AnnulusGames <annulusgames@gmail.com>
2024-03-02 16:24:03 +0900

    Fix: IJobArchChunk.Schedule causes crash

--- src/Arch.Unity/Assets/Sandbox/TestBootstrap.cs
+++ src/Arch.Unity/Assets/Sandbox/TestBootstrap.cs
@@ -1,7 +1,11 @@
 using Arch.Core;
+using Arch.Core.Utils;
 using Arch.System;
+using Arch.Unity.Jobs;
 using Arch.Unity.Toolkit;
 using R3;
+using Unity.Burst;
+using Unity.Collections;
 using UnityEngine;
 
 public static class TestBootstrap
@@ -19,16 +23,37 @@ public static class TestBootstrap
     }
 }
 
-public class User
+public struct User
 {
-    public User(string name)
+    public User(FixedString64Bytes name)
     {
         this.Name = name;
     }
 
-    public string Name;
+    public FixedString64Bytes Name;
 }
 
+public struct Score
+{
+    public int Value;
+}
+
+[BurstCompile]
+public struct IncrementScoreJob : IJobArchChunk
+{
+    public int Id;
+
+    public void Execute(NativeChunk chunk)
+    {
+        var array = chunk.GetNativeArray<Score>(Id);
+        for (int i = 0; i < array.Length; i++)
+        {
+            var id = array[i];
+            id.Value++;
+            array[i] = id;
+        }
+    }
+}
 
 public sealed partial class HelloSystem : BaseSystem<World, SystemState>
 {
@@ -36,8 +61,18 @@ public sealed partial class HelloSystem : BaseSystem<World, SystemState>
 
     public override void Initialize()
     {
-        World.Create(new User("Alice"));
-        World.Create(new User("Bob"));
+        World.Create(new User("Alice"), new Score() { Value = 1 });
+        World.Create(new User("Bob"), new Score() { Value = 2 });
+    }
+
+    public override void BeforeUpdate(in SystemState t)
+    {
+        var job = new IncrementScoreJob()
+        {
+            Id = Component<Score>.ComponentType.Id
+        };
+        job.Schedule(World, new QueryDescription().WithAll<Score>())
+            .Complete();
     }
 
     [Query]
