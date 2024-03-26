637db0022e20fc559f36c73392846ce56b6edee3
AnnulusGames <annulusgames@gmail.com>
2024-03-27 00:13:10 +0900

    Add: EntityConversion.Convert

--- src/Arch.Unity/Assets/Arch.Unity/Runtime/Conversion/EntityConversion.cs
+++ src/Arch.Unity/Assets/Arch.Unity/Runtime/Conversion/EntityConversion.cs
@@ -1,10 +1,44 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityComponent = UnityEngine.Component;
 using Arch.Core;
+using Arch.Core.Utils;
+using System;
 
 namespace Arch.Unity.Conversion
 {
     public static class EntityConversion
     {
+        sealed class Converter : IEntityConverter
+        {
+            readonly List<ComponentType> componentTypes = new();
+            readonly List<object> components = new();
+
+            public void AddComponent<T>(T component)
+            {
+                if (typeof(T).IsValueType)
+                {
+                    componentTypes.Add(typeof(T));
+                }
+                else
+                {
+                    componentTypes.Add(component.GetType());
+                }
+
+                components.Add(component); // TODO: avoid boxing
+            }
+
+            public Entity Convert(World world)
+            {
+                var entity = world.Create(componentTypes.ToArray());
+                foreach (var component in components)
+                {
+                    world.Set(entity, component);
+                }
+                return entity;
+            }
+        }
+
 #if !ARCH_UNITY_DISABLE_AUTOMATIC_BOOTSTRAP
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
         static void Init()
@@ -17,6 +51,50 @@ namespace Arch.Unity.Conversion
             };
         }
 #endif
+
+        public static EntityReference Convert(GameObject gameObject, ConversionMode conversionMode = ConversionMode.ConvertAndDestroy, bool convertHybridComponents = false)
+        {
+            return Convert(gameObject, DefaultWorld, conversionMode, convertHybridComponents);
+        }
+
+        public static EntityReference Convert(GameObject gameObject, World world, ConversionMode conversionMode = ConversionMode.ConvertAndDestroy, bool convertHybridComponents = false)
+        {
+            var components = gameObject.GetComponents<UnityComponent>();
+            var converter = new Converter();
+
+            converter.AddComponent(new GameObjectReference(gameObject));
+
+            for (int i = 0; i < components.Length; i++)
+            {
+                var component = components[i];
+
+                if (component is IComponentConverter componentConverter)
+                {
+                    try
+                    {
+                        componentConverter.Convert(converter);
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.LogException(ex);
+                    }
+                }
+                else if (conversionMode == ConversionMode.SyncWithEntity && convertHybridComponents)
+                {
+                    converter.AddComponent((object)component);
+                }
+            }
+
+            world ??= DefaultWorld;
+            var entityReference = world.Reference(converter.Convert(world));
+
+            if (conversionMode == ConversionMode.ConvertAndDestroy)
+            {
+                UnityEngine.Object.Destroy(gameObject);
+            }
+
+            return entityReference;
+        }
 
         public static World DefaultWorld { get; set; }
     }
--- src/Arch.Unity/Assets/Arch.Unity/Runtime/Conversion/EntityConverter.cs
+++ src/Arch.Unity/Assets/Arch.Unity/Runtime/Conversion/EntityConverter.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
-using Arch.Core;
-using Arch.Core.Utils;
 using UnityEngine;
-using UnityComponent = UnityEngine.Component;
+using Arch.Core;
 
 namespace Arch.Unity.Conversion
 {
@@ -11,35 +7,6 @@ namespace Arch.Unity.Conversion
     [DefaultExecutionOrder(-100)]
     public sealed class EntityConverter : MonoBehaviour
     {
-        sealed class Converter : IEntityConverter
-        {
-            readonly List<ComponentType> componentTypes = new();
-            readonly List<object> components = new();
-
-            public void AddComponent<T>(T component)
-            {
-                if (typeof(T).IsValueType)
-                {
-                    componentTypes.Add(typeof(T));
-                }
-                else
-                {
-                    componentTypes.Add(component.GetType());
-                }
-
-                components.Add(component); // TODO: avoid boxing
-            }
-
-            public Entity Convert(World world)
-            {
-                var entity = world.Create(componentTypes.ToArray());
-                foreach (var component in components)
-                {
-                    world.Set(entity, component);
-                }
-                return entity;
-            }
-        }
         [SerializeField] ConversionMode conversionMode;
         [SerializeField] bool convertHybridComponents;
 
@@ -49,39 +16,7 @@ namespace Arch.Unity.Conversion
         void Awake()
         {
             world = EntityConversion.DefaultWorld;
-
-            var components = GetComponents<UnityComponent>();
-            var converter = new Converter();
-
-            converter.AddComponent(new GameObjectReference(gameObject));
-
-            for (int i = 0; i < components.Length; i++)
-            {
-                var component = components[i];
-
-                if (component is IComponentConverter componentConverter)
-                {
-                    try
-                    {
-                        componentConverter.Convert(converter);
-                    }
-                    catch (Exception ex)
-                    {
-                        Debug.LogException(ex);
-                    }
-                }
-                else if (conversionMode == ConversionMode.SyncWithEntity && convertHybridComponents)
-                {
-                    converter.AddComponent((object)component);
-                }
-            }
-
-            entityReference = world.Reference(converter.Convert(world));
-
-            if (conversionMode == ConversionMode.ConvertAndDestroy)
-            {
-                Destroy(gameObject);
-            }
+            entityReference = EntityConversion.Convert(gameObject, world, conversionMode, convertHybridComponents);
         }
 
         void OnDestroy()
