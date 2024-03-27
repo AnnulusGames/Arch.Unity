27fa6877c13712962e3f105d774c1aa6a6698476
AnnulusGames <annulusgames@gmail.com>
2024-03-27 12:23:33 +0900

    Change: implements IEquatable<T>

--- src/Arch.Unity/Assets/Arch.Unity/Runtime/Toolkit/SystemState.cs
+++ src/Arch.Unity/Assets/Arch.Unity/Runtime/Toolkit/SystemState.cs
@@ -1,8 +1,26 @@
+using System;
+
 namespace Arch.Unity.Toolkit
 {
-    public struct SystemState
+    public struct SystemState : IEquatable<SystemState>
     {
         public float DeltaTime;
         public double Time;
+
+        public readonly bool Equals(SystemState other)
+        {
+            return other.DeltaTime == DeltaTime && other.Time == Time;
+        }
+
+        public override readonly bool Equals(object obj)
+        {
+            if (obj is SystemState state) return Equals(state);
+            return false;
+        }
+
+        public override readonly int GetHashCode()
+        {
+            return HashCode.Combine(DeltaTime, Time);
+        }
     }
 }
