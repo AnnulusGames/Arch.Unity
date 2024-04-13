071014014387afc9b0e407d002448c0bbfdcf9d6
AnnulusGames <annulusgames@gmail.com>
2024-04-13 16:07:24 +0900

    Update: sandbox

--- src/Arch.Unity/Assets/Sandbox/TestBootstrap.cs
+++ src/Arch.Unity/Assets/Sandbox/TestBootstrap.cs
@@ -1,11 +1,11 @@
 using Arch.Core;
 using Arch.Core.Utils;
 using Arch.System;
+using Arch.Unity;
 using Arch.Unity.Jobs;
 using Arch.Unity.Toolkit;
 using R3;
 using Unity.Burst;
-using Unity.Collections;
 using UnityEngine;
 
 public static class TestBootstrap
@@ -22,17 +22,6 @@ public static class TestBootstrap
             .RegisterTo(Application.exitCancellationToken);
     }
 }
-
-public struct User
-{
-    public User(FixedString64Bytes name)
-    {
-        this.Name = name;
-    }
-
-    public FixedString64Bytes Name;
-}
-
 public struct Score
 {
     public int Value;
@@ -61,8 +50,8 @@ public sealed partial class HelloSystem : BaseSystem<World, SystemState>
 
     public override void Initialize()
     {
-        World.Create(new User("Alice"), new Score() { Value = 1 });
-        World.Create(new User("Bob"), new Score() { Value = 2 });
+        World.Create(new EntityName("Alice"), new Score() { Value = 1 });
+        World.Create(new EntityName("Bob"), new Score() { Value = 2 });
     }
 
     public override void BeforeUpdate(in SystemState t)
@@ -76,8 +65,8 @@ public sealed partial class HelloSystem : BaseSystem<World, SystemState>
     }
 
     [Query]
-    public void Hello(ref User user)
+    public void Hello(ref EntityName name)
     {
-        Debug.Log($"Hello, {user.Name}!");
+        Debug.Log($"Hello, {name}!");
     }
 }
